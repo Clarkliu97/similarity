d2c9a47e2f837804fa370d7e6f0ecbd992d9dde0
junaid8901 <murshad8901@gmail.com>
2023-01-05 18:32:11 +0500

    update task

--- similarity/documentchecker/tasks.py
+++ similarity/documentchecker/tasks.py
@@ -6,6 +6,31 @@ from django.db.models import Sum
 from itertools import permutations
 
 
+def remove_duplicate_files(file_ids):
+    # Create a new list to store the unique files
+    unique_files = []
+    main_list=[]
+    # Iterate over the list of file IDs
+    for file_id in file_ids:
+        # Get the file object
+        file = File.objects.get(pk=file_id)
+
+        # Check if a file with the same creation time already exists in the unique_files list
+        duplicate_found = False
+        for unique_file in unique_files:
+            if file.created_at == unique_file.created_at:
+                duplicate_found = True
+                break
+
+        # If no duplicate was found, add the file to the unique_files list
+        if not duplicate_found:
+            unique_files.append(file)
+            main_list.append(file.id)
+            
+
+    # Return the list of unique files
+    return main_list
+
 
 
 def extracttext(file):
@@ -17,12 +42,13 @@ def extracttext(file):
 @app.task()
 def similaritycheck(*args, **kwargs):
     id = kwargs['id']
-    
+    prog_id=kwargs['progress_id']
+    print("prog_id",id)
     similarity_obj=SimilarityCheck.objects.get(id=id)
-    progress_obj=Progress()
-    progress_obj.task=similarity_obj
+    progress_obj=Progress.objects.get(id=prog_id)
+    # progress_obj.task=similarity_obj
     
-    progress_obj.save()
+    # progress_obj.save()
     config = Threshold.objects.get()
     
     list_of_id = kwargs['file']
@@ -31,26 +57,7 @@ def similaritycheck(*args, **kwargs):
     author     =kwargs['author']
     objts=File.objects.filter(author=author,id__in=list_of_id)
     file_ids = [obj.id for obj in objts]
-    years = [ year[0].year for year in objts.values_list('created_at')]
-    distinct_years = list(set(years))
-    data_dict = {}
-    year_info_list=[]
-    for year in distinct_years:
-        
-        
-        data_dict["year"] = year
-        year_querset=objts.filter(created_at__year=year)
-        file_per_year=year_querset.values_list('file').count()
-        data_dict['file_count'] = file_per_year
-        word_count_per_year = year_querset.aggregate(words_count=Sum('word_count'))
-        data_dict['word_count'] = word_count_per_year['words_count']
-        year_info_list.append(data_dict)
-        
-        
-    similarity_obj.year_info= year_info_list
-    similarity_obj.save()
     pass_list=[]
-    
     list_perm = [item for item in permutations(file_ids, r = 2)]
     for i,item in enumerate(list_perm):
         try:
@@ -80,33 +87,54 @@ def similaritycheck(*args, **kwargs):
             if first_element.created_at > Seocnd_element.created_at:
                 pass_list.append(Seocnd_element.id)
             else:
-                pass_list.append(first_obj.id)
+                pass_list.append(first_element.id)
             
         j=i+1   
         progress=round(j/len(list_perm),1) * 100
         progress_obj.progress=progress
         progress_obj.status='Inprogress'
         similarity_obj.save()
+        
+    unique_files_id = remove_duplicate_files(pass_list)
+    unique_files_id= list(set(unique_files_id)) 
+    print("last",unique_files_id)
     
-    pass_list= list(set(pass_list))   
-
     progress_obj.threshold=config.min_file
     progress_obj.save()
-    if progress_obj.progress==100.0:
-        similarity_obj.pass_file =len(pass_list)    
+    
+    
+    year_objects=File.objects.filter(author=author,id__in=unique_files_id)
+    years = [ year[0].year for year in year_objects.values_list('created_at')]
+    distinct_years = list(set(years))
+    
+    data_dict = {}
+    year_info_list=[]
+    for year in distinct_years:
         
-        if pass_list:
-            for id in pass_list:
+        data_dict["year"] = year
+        year_querset=year_objects.filter(created_at__year=year)
+        file_per_year=year_querset.values_list('file').count()
+        data_dict['file_count'] = file_per_year
+        word_count_per_year = year_querset.aggregate(words_count=Sum('word_count'))
+        data_dict['word_count'] = word_count_per_year['words_count']
+        year_info_list.append(data_dict)
+        
+    
+    similarity_obj.year_info= year_info_list
+    similarity_obj.save()
+    print("year",similarity_obj.year_info)
+    if progress_obj.progress==100.0:
+        
+        if unique_files_id:
+            for id in unique_files_id:
                 similarity_obj.file.add(id)        
-                similarity_obj.save()
-
-            
+                similarity_obj.save()      
                 
                 progress_obj.completed_file +=1
                 progress_obj.save()
-    if progress_obj.completed_file == len(pass_list):
+    
+    if progress_obj.completed_file == len(unique_files_id):
         progress_obj.status='Complete'
-        progress_obj.complete=True
         progress_obj.save()
         
     
