ab38295f88c2272e33a3404d96d44895bf81ab3b
mejazgithub <ejazsheikh.403@gmail.com>
2023-01-18 15:25:24 +0500

    moved choices from model to utils

--- similarity/documentchecker/utils.py
+++ similarity/documentchecker/utils.py
@@ -3,6 +3,29 @@ import tempfile
 from docx import Document
 
 
+file_error_choices = [
+    (0, 'File name not valid'),
+    (1, 'File is corrupted'),
+    (2, 'Invalid file extension'),
+    (3, 'Created_at date not found'),
+    (4, 'Invalid created_at date format'),
+    (5, 'File without words'),
+    (6, 'File without aurthor')
+]
+
+report_choices = [
+    ('Complete', 'Complete'),
+    ('Failed', 'Failed'),
+    ('Inprogress', 'Inprogress'),]
+
+task_error_choices = [
+    (1, "File not found"),
+    (2, "No file with unique content"),
+    (3, "Unknown author"),
+    (4, "Threshold not found"),
+    (5, "Error in file"),
+]
+
 def get_doc_text(file):
     
     tempf, tempfn = tempfile.mkstemp(suffix='.doc')
