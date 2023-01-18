e571ab1582025dd2204ef37e61f00e141cc61db6
mejazgithub <ejazsheikh.403@gmail.com>
2023-01-18 12:33:57 +0500

    removed file checks and added new checks in task

--- similarity/documentchecker/views.py
+++ similarity/documentchecker/views.py
@@ -1,36 +1,15 @@
-import os
-import re
-import olefile
 from django.http import HttpResponse
-from dateutil import parser
 from rest_framework import generics, status
 from rest_framework.response import Response
-from docx import Document
 from .models import Task,File
 from .tasks import similaritycheck
 from .serializers import FileSerializer,TaskSerialiazer
 from django.shortcuts import render
+import json
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
-
-
-def words_count(docText):
-    
-    return(len(re.findall(r'\w+',docText)))
-
-def get_doc_text(file):
-    import tempfile
-    tempf, tempfn = tempfile.mkstemp(suffix='.doc')
-    for chunk in file.chunks():
-        os.write(tempf, chunk)
-    from subprocess import Popen, PIPE
-    cmd = ['antiword', tempfn]
-    p = Popen(cmd, stdout=PIPE)
-    stdout, _ = p.communicate()
-    text =  stdout.decode('ascii', 'ignore')
-    return text
 
 ############################################################################################
 class UploadFile(generics.ListAPIView,generics.GenericAPIView):
@@ -42,66 +21,15 @@ class UploadFile(generics.ListAPIView,generics.GenericAPIView):
     
 
     def post(self, request):
-        
-        file = request.data.get("file", None)
+        file = request.data.get('file', None)
         if file is None or file=='':
             return Response({"file": "file is required"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        extension = os.path.splitext(str(file))[-1].lower()
-        if extension == ".docx":
-            
-            try:
-                doc = Document(file)
-            except Exception as e:
-                return Response({"file":"File is Corrupted ".format(e)},status=status.HTTP_400_BAD_REQUEST)
-            
-            docText = '\n\n'.join(paragraph.text for paragraph in doc.paragraphs)
-            prop = doc.core_properties
-            
-            
-            author=prop.author
-            creation_date=prop.created
-            word_count=words_count(docText)
-            
-        elif extension=='.doc':
-            ole = olefile.OleFileIO(file)
-            metadata = ole.get_metadata()
-            creation_date=metadata.create_time
-            author=metadata.author
-            if author != None and author == '':
-                author=author.decode("utf-8") 
-            else:
-                author=None
-            text = get_doc_text(file)
-            word_count=words_count(text)
 
-        else:
-            return Response({"file":"File Not Valid"}, status=status.HTTP_400_BAD_REQUEST)
-     
-        try:
-            datetime = parser.parse(str(creation_date))
-        except Exception as e:
-            return Response({"file":"date formate {} ".format(e)},status=status.HTTP_400_BAD_REQUEST)
-
-        if int(word_count) == 0:
-            return Response({'file': 'file without words not allowed {}'.format(file)}, status=status.HTTP_400_BAD_REQUEST)
-            
-            
-        data_dict={"file": file, 
-                    'author':author,
-                    "created_at":datetime,
-                    'word_count':word_count
-                    }
-            
-        if creation_date is None or creation_date=='':
-            return Response({"Year": "File Without Year Not Allowed {}".format(file)}, status=status.HTTP_400_BAD_REQUEST)
-        if author is None or author=='':
-            return Response({"Author": "{} does not have author ".format(file)}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = self.serializer_class(data=data_dict,context={"request": self.request})
+        serializer = self.serializer_class(data=request.data,context={"request": self.request})
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data_dict, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     
     def get(self, request, *args, **kwargs):
@@ -121,25 +49,27 @@ class TaskView(generics.RetrieveAPIView,  generics.UpdateAPIView,generics.Generi
     lookup_field = 'id'
     
     def post(self, request):
-        file = request.data.get("file_id")
-        if len(file) < 2:
+        files = request.data.get("file_id")
+        authors = request.data.get('authors')
+        if len(files) < 2:
             return Response({"file":"Add more files"},status=status.HTTP_400_BAD_REQUEST)
-        if file is None or file==[]:
+        if files is None or files==[]:
             return Response({"file":"Select atleast two file"},status=status.HTTP_400_BAD_REQUEST)
-        author = request.data.get("author")
-        if author is None or author=="":
-            return Response("Please Select the Author",status=status.HTTP_400_BAD_REQUEST)
+        if authors is None or len(authors) == 0:
+            return Response({"author":"Select atleast one author"},status=status.HTTP_400_BAD_REQUEST)
         
         
-        objts=objts=File.objects.filter(author=author,id__in=file)
+        objts=File.objects.filter(author__in=authors,id__in=files)
         if len(objts)<=1:
-            return Response({"file":"Select atleast two files for {}".format(author)},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"file":"Select atleast two files for {}".format(authors)},status=status.HTTP_400_BAD_REQUEST)
+
         # create and task object
         SimilarityCheck_obj=Task()
-        SimilarityCheck_obj.author=author
+        SimilarityCheck_obj.authors=authors
         SimilarityCheck_obj.save()
+
         # call celery
-        similaritycheck.delay(file=file,author=author,id=SimilarityCheck_obj.id)
+        similaritycheck.delay(file=files,authors=authors,id=SimilarityCheck_obj.id)
         
         return Response({"task_id":SimilarityCheck_obj.id },status=status.HTTP_200_OK)
 
@@ -157,7 +87,8 @@ class TaskView(generics.RetrieveAPIView,  generics.UpdateAPIView,generics.Generi
         if completed_file < threshold_file:
             return Response({"file":"upload more file "},status=status.HTTP_400_BAD_REQUEST)        
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response(serializer.data)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
