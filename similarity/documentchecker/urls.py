7821ef6e20ad9836e1f44872bdfba094bcc4e5ea
mejazgithub <ejazsheikh.403@gmail.com>
2023-01-18 12:33:05 +0500

    fixed spelling mistaske

--- similarity/documentchecker/urls.py
+++ similarity/documentchecker/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     
     path('file/<int:id>/',                             UploadFile.as_view(),                      name='get-view'),
     path('file/',                                     UploadFile.as_view(),                      name='fileupload-view'),
-    path('task/',                                     TaskView.as_view(),                        name='dacument_similerity-view'),
-    path('task/<int:id>/',                            TaskView.as_view(),                        name='similerity-detail-view'),
+    path('task/',                                     TaskView.as_view(),                        name='document-similarity-view'),
+    path('task/<int:id>/',                            TaskView.as_view(),                        name='similarity-detail-view'),
     
 ]
 
